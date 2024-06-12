5eb78f5b72d3088bd04fb807c0136c6dc2c7c0b0
sulthanfyt <sulthan.fairuzandy3@upi.edu>
2024-06-12 19:14:13 +0700

    Updated interface

---
+++ app/src/main/java/com/dicoding/kaloriku/ui/auth/viewmodel/BMIViewModel.kt
@@ -0,0 +1,45 @@
+package com.dicoding.kaloriku.ui.auth.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dicoding.kaloriku.data.pref.UserRepository
+import com.dicoding.kaloriku.data.response.BMIResponse
+import com.dicoding.kaloriku.data.retrofit.ApiConfig
+import com.dicoding.kaloriku.data.retrofit.ApiService
+import kotlinx.coroutines.launch
+
+
+
+class BMIViewModel(private val userRepository: UserRepository) : ViewModel() {
+
+    private val apiService = ApiConfig.getApiService() // Access ApiService directly
+
+    private val _bmiResult = MutableLiveData<BMIResponse>()
+    val bmiResult: LiveData<BMIResponse> = _bmiResult
+
+    fun calculateBMI(userId: String) {
+        viewModelScope.launch {
+            try {
+                userRepository.getToken().collect { token ->
+                    val response = apiService.calculateBMI(token, userId)
+                    println("$token")
+                    if (response.isSuccessful) {
+                        val bmiResponse = response.body()
+                        bmiResponse?.let {
+                            _bmiResult.value = it
+                            Log.d("BMIViewModel", "BMI: ${it.bmi}, Category: ${it.category}")
+                        }
+                    } else {
+                        Log.e("BMIViewModel", "Response error: ${response.errorBody()?.string()}")
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e("BMIViewModel", "Error fetching BMI data", e)
+            }
+        }
+    }
+}
+
